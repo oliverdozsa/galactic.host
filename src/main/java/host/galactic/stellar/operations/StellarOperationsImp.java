ce735600f7f6d0b191029ccbaea2b6912a1c3857
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-05-27 07:33:20 +0200

    Statred XLM transfer Stellar operation.

--- src/main/java/host/galactic/stellar/operations/StellarOperationsImp.java
+++ src/main/java/host/galactic/stellar/operations/StellarOperationsImp.java
@@ -2,17 +2,21 @@ package host.galactic.stellar.operations;
 
 import io.smallrye.mutiny.Uni;
 import io.quarkus.logging.Log;
-import org.stellar.sdk.KeyPair;
+import org.stellar.sdk.*;
+import org.stellar.sdk.operations.PaymentOperation;
+
+import java.math.BigDecimal;
+
+import static org.stellar.sdk.AbstractTransaction.MIN_BASE_FEE;
 
 class StellarOperationsImp implements StellarOperations {
-    private final String url;
+    private final Server server;
+    private final Network network;
 
     public StellarOperationsImp(boolean isOnTestNet) {
-        if(isOnTestNet) {
-            url = "https://horizon-testnet.stellar.org";
-        } else {
-            url = "https://horizon.stellar.org";
-        }
+        String serverUri = isOnTestNet ? "https://horizon-testnet.stellar.org" : "https://horizon.stellar.org";
+        server = new Server(serverUri);
+        network = isOnTestNet ? Network.TESTNET : Network.PUBLIC;
     }
 
     public Uni<Void> transferXlmFrom(String sourceAccountSecret, double xlm, String targetAccountSecret) {
@@ -20,8 +24,32 @@ class StellarOperationsImp implements StellarOperations {
         String targetAccountId = toTruncatedAccountId(targetAccountSecret);
 
         Log.infof("transferXlmFrom(): Transferring: %s -> %s XLMs -> %s", sourceAccountId, xlm, targetAccountId);
-        // TODO
+
+        TransactionBuilderAccount sourceAccount = server.loadAccount(sourceAccountId);
+
+        PaymentOperation paymentOperation = PaymentOperation.builder()
+                .destination(targetAccountId)
+                .asset(Asset.createNativeAsset())
+                .amount(new BigDecimal(xlm))
+                .build();
+
+        Transaction transaction = new TransactionBuilder(sourceAccount, network)
+                .setBaseFee(MIN_BASE_FEE)
+                .setTimeout(30)
+                .addOperation(paymentOperation)
+                .build();
+
+        KeyPair sourceKeyPair = KeyPair.fromSecretSeed(sourceAccountSecret);
+        transaction.sign(sourceKeyPair);
+
+        server.submitTransaction(transaction);
+
+        // TODO: Wrap operation in a Uni
         return Uni.createFrom().voidItem();
+    }
+
+    public void done() {
+        server.close();
     }
 
     private static String toTruncatedAccountId(String accountSecret) {
