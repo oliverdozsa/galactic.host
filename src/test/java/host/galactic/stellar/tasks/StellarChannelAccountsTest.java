3bcf8b480cbee916651eb4c9627e7f228e00a380
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-08-24 09:06:29 +0200

    Started tests for channel accounts.

---
+++ src/test/java/host/galactic/stellar/tasks/StellarChannelAccountsTest.java
@@ -0,0 +1,61 @@
+package host.galactic.stellar.tasks;
+
+import host.galactic.stellar.rest.StellarRestTestBase;
+import host.galactic.stellar.rest.requests.voting.AddVotersRequest;
+import io.quarkus.logging.Log;
+import io.restassured.http.ContentType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class StellarChannelAccountsTest extends StellarRestTestBase {
+    @Test
+    public void testChannelAccountsCreated() {
+        Log.info("[START TEST]: testChannelAccountsCreated()");
+
+        createAVotingWithThreeParticipants();
+        Assertions.fail("Implement testChannelAccountsCreated()");
+
+        Log.info("[  END TEST]: testChannelAccountsCreated()");
+    }
+
+    @Test
+    public void testChannelAccountsAreRefundedWhenVotingIsDeleted() {
+        Log.info("[START TEST]: testChannelAccountsAreRefundedWhenVotingIsDeleted()");
+
+        createAVotingWithThreeParticipants();
+        Assertions.fail("Implement testChannelAccountsAreRefundedWhenVotingIsDeleted()");
+
+        Log.info("[  END TEST]: testChannelAccountsAreRefundedWhenVotingIsDeleted()");
+    }
+
+    @Test
+    public void testChannelAccountsAreRefundedWhenVotingEnds() {
+        Log.info("[START TEST]: testChannelAccountsAreRefundedWhenVotingEnds()");
+
+        createAVotingWithThreeParticipants();
+        Assertions.fail("Implement testChannelAccountsAreRefundedWhenVotingEnds()");
+
+        Log.info("[  END TEST]: testChannelAccountsAreRefundedWhenVotingEnds()");
+    }
+
+    private long createAVotingWithThreeParticipants() {
+        var id = createAVotingAs("alice");
+
+        AddVotersRequest addVotersRequest = new AddVotersRequest(List.of("emily@galactic.pub", "duke@galactic.pub", "alice@galactic.pub"));
+        String withAccessTokenForAlice = authForTest.loginAs("alice");
+        given()
+                .auth().oauth2(withAccessTokenForAlice)
+                .contentType(ContentType.JSON)
+                .body(addVotersRequest)
+                .post(stellarVotingRest + "/addvoters/" + id)
+                .then()
+                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
+
+        return id;
+    }
+}
