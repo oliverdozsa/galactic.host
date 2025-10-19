d2aff5cc9cb48369f801633b78d03f22b4d43d72
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-10-19 21:47:38 +0200

    Actual stellar operations for channel accounts, part 1.

--- src/main/java/host/galactic/stellar/operations/StellarChannelAccountOperation.java
+++ src/main/java/host/galactic/stellar/operations/StellarChannelAccountOperation.java
@@ -1,4 +1,48 @@
 package host.galactic.stellar.operations;
 
+import io.smallrye.mutiny.Uni;
+import org.stellar.sdk.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StellarChannelAccountOperation {
+    private Server server;
+    private Network network;
+
+    public StellarChannelAccountOperation(Server server, Network network) {
+        this.server = server;
+        this.network = network;
+    }
+
+    public Uni<List<StellarChannelAccount>> create(StellarChannelAccountOperationPayload payload) {
+        return Uni.createFrom().item(() -> {
+            var generatorKeyPair = KeyPair.fromSecretSeed(payload.generatorAccountSecret());
+            var generatorAccount = server.loadAccount(generatorKeyPair.getAccountId());
+            var transactionBuilder = new TransactionBuilder(generatorAccount, network);
+
+            var channelAccounts = new ArrayList<StellarChannelAccount>();
+
+            for(var i = 0; i < payload.numOfAccountsToCreate(); i++) {
+                var channelKeyPair = prepareAccountCreationOn(transactionBuilder);
+                channelAccounts.add(from(channelKeyPair, payload.votingId()));
+            }
+
+            // TODO
+
+            return null;
+        });
+    }
+
+    private KeyPair prepareAccountCreationOn(TransactionBuilder txBuilder) {
+        var channelKeyPair = KeyPair.random();
+
+        // TODO
+
+        return channelKeyPair;
+    }
+
+    private StellarChannelAccount from(KeyPair keyPair, Long votingId) {
+        return new StellarChannelAccount(new String(keyPair.getSecretSeed()), votingId);
+    }
 }
