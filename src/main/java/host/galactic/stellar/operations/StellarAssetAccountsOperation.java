934db44f6a9c9c4a148afad1d1d0df1e04fbcde3
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-11-30 20:49:06 +0100

    Stellar asset account operation, continued.

--- src/main/java/host/galactic/stellar/operations/StellarAssetAccountsOperation.java
+++ src/main/java/host/galactic/stellar/operations/StellarAssetAccountsOperation.java
@@ -1,10 +1,9 @@
 package host.galactic.stellar.operations;
 
+import host.galactic.data.entities.VotingEntity;
 import io.smallrye.mutiny.Uni;
-import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.Network;
-import org.stellar.sdk.Server;
-import org.stellar.sdk.TransactionBuilder;
+import org.stellar.sdk.*;
+import org.stellar.sdk.xdr.Transaction;
 
 import java.math.BigDecimal;
 
@@ -12,33 +11,63 @@ public class StellarAssetAccountsOperation {
     private Server server;
     private Network network;
 
+    private KeyPair distributionKeyPair;
+    private KeyPair ballotKeyPair;
+    private KeyPair issuerKeyPair;
+
+    private TransactionBuilder txBuilder;
+
+    private VotingEntity voting;
+
     public StellarAssetAccountsOperation(Server server, Network network) {
         this.server = server;
         this.network = network;
     }
 
     public Uni<StellarAssetAccounts> create(StellarAssetAccountsOperationPayload payload) {
+        voting = payload.votingEntity();
+
         var fundingKeyPair = KeyPair.fromSecretSeed(payload.fundingSecret());
         var fundingAccount = server.loadAccount(fundingKeyPair.getAccountId());
-
-        var transactionBuilder = new TransactionBuilder(fundingAccount, network);
+        txBuilder = new TransactionBuilder(fundingAccount, network);
 
         var distributionStartingBalance = new BigDecimal(payload.votingEntity().maxVoters * 2);
-        var distributionKeyPair = prepareAccountCreationOn(transactionBuilder, distributionStartingBalance);
+        distributionKeyPair = prepareAccountCreation(distributionStartingBalance);
+        ballotKeyPair = prepareAccountCreation(new BigDecimal(2));
+        issuerKeyPair = prepareAccountCreation(new BigDecimal(2));
 
-        var ballotKeyPair = prepareAccountCreationOn(transactionBuilder, new BigDecimal(2));
-        var issuerKeyPair = prepareAccountCreationOn(transactionBuilder, new BigDecimal(2));
+        prepareToken();
 
         // TODO
 
         return null;
     }
 
-    private KeyPair prepareAccountCreationOn(TransactionBuilder txBuilder, BigDecimal startingBalanceXlm) {
+    private KeyPair prepareAccountCreation(BigDecimal startingBalanceXlm) {
         var newAccount = KeyPair.random();
 
         // TODO
 
         return newAccount;
+    }
+
+    private void prepareToken() {
+        allowAccountsToHaveVoteTokens(distributionKeyPair, ballotKeyPair);
+        sendAllVoteTokenToDistribution();
+        lockIssuer();
+    }
+
+    private void allowAccountsToHaveVoteTokens(KeyPair... accounts) {
+        var asset = Asset.create(null, voting.assetCode, issuerKeyPair.getAccountId());
+
+        // TODO
+    }
+
+    private void sendAllVoteTokenToDistribution() {
+        // TODO
+    }
+
+    private void lockIssuer() {
+        // TODO
     }
 }
