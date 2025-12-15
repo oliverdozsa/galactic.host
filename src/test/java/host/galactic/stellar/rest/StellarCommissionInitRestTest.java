a048d81b373709a59460f97b88fb53b99bfeff82
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-12-15 20:58:46 +0100

    Started commission init.

---
+++ src/test/java/host/galactic/stellar/rest/StellarCommissionInitRestTest.java
@@ -0,0 +1,34 @@
+package host.galactic.stellar.rest;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@QuarkusTest
+public class StellarCommissionInitRestTest {
+    @Test
+    public void testInitSession() {
+        fail("Implement testInitSession()");
+    }
+
+    @Test
+    public void testInitSessionVotingDoesNotExists() {
+        fail("Implement testInitSessionVotingDoesNotExists()");
+    }
+
+    @Test
+    public void testInitSessionUserIsNotParticipant() {
+        fail("Implement testInitSessionUserIsNotParticipant()");
+    }
+
+    @Test
+    public void testInitSessionVotingIsNotInitializedFully() {
+        fail("Implement testInitSessionVotingIsNotInitializedFully()");
+    }
+
+    @Test
+    public void testInitSessionUserIsNotAuthorizedToInit() {
+        fail("Implement testInitSessionUserIsNotParticipant()");
+    }
+}
