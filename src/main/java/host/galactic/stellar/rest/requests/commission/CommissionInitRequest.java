3ebc474463bb6eeaac3f3e1af8dbd688b9deb272
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-12-16 16:43:26 +0100

    Started tests on commission init.

---
+++ src/main/java/host/galactic/stellar/rest/requests/commission/CommissionInitRequest.java
@@ -0,0 +1,4 @@
+package host.galactic.stellar.rest.requests.commission;
+
+public class CommissionInitRequest {
+}
--- src/test/java/host/galactic/stellar/rest/StellarCommissionInitRestTest.java
+++ src/test/java/host/galactic/stellar/rest/StellarCommissionInitRestTest.java
@@ -1,15 +1,43 @@
 package host.galactic.stellar.rest;
 
+import host.galactic.stellar.rest.requests.commission.CommissionInitRequest;
+import host.galactic.stellar.rest.responses.commission.CommissionInitResponse;
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @QuarkusTest
-public class StellarCommissionInitRestTest {
+public class StellarCommissionInitRestTest extends StellarRestTestBase {
     @Test
     public void testInitSession() {
-        fail("Implement testInitSession()");
+        Log.info("[START TEST]: testInitSession()");
+
+        var votingId = createAVotingAs("alice");
+        addVoterAsParticipantTo(votingId, "charlie", "alice");
+
+        var withAccessToken = authForTest.loginAs("charlie");
+
+        var initRequest = new CommissionInitRequest();
+
+        var response = given()
+                .auth().oauth2(withAccessToken)
+                .contentType(ContentType.JSON)
+                .body(initRequest)
+                .when()
+                .post(stellarCommissionRest + "/initsession")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().body()
+                .as(CommissionInitResponse.class);
+
+        // TODO: asserts about response
+
+        Log.info("[  END TEST]: testInitSession()");
     }
 
     @Test
