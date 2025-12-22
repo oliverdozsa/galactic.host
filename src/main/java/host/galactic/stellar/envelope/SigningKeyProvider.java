33306b7392f6d22ca1c905b8616476a6ee5f7eec
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-12-22 12:08:35 +0100

    Started RSA signing key handling.

---
+++ src/main/java/host/galactic/stellar/envelope/SigningKeyProvider.java
@@ -0,0 +1,83 @@
+package host.galactic.stellar.envelope;
+
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.LaunchMode;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Named;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
+import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.util.PrivateKeyInfoFactory;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class SigningKeyProvider {
+    private AsymmetricCipherKeyPair keyPair;
+
+
+    @Produces
+    @Named("signing")
+    AsymmetricCipherKeyPair provide() {
+        if(keyPair == null) {
+            readKeyPair();
+        }
+
+        return keyPair;
+    }
+
+    private void readKeyPair() {
+        var keyPairAsPem = getKeyPairPem();
+        if(keyPairAsPem == null || keyPairAsPem.isBlank()) {
+            Log.error("galactic.host.voting.signing.key cannot be empty!");
+            throw new RuntimeException("galactic.host.voting.signing.key cannot be empty!");
+        }
+
+        // TODO
+    }
+
+    private String getKeyPairPem() {
+        if(LaunchMode.current() != LaunchMode.NORMAL) {
+            Log.info("Generating random signing keypair for dev and test modes.");
+            var keyPair = generateKeyPair();
+            return privateToPemString(keyPair);
+        } else {
+            return ConfigProvider.getConfig().getValue("galactic.host.voting.signing.key", String.class);
+        }
+    }
+
+    private AsymmetricCipherKeyPair generateKeyPair() {
+        RSAKeyPairGenerator generator = new RSAKeyPairGenerator();
+
+        BigInteger publicExponent = new BigInteger("10001", 16);
+        SecureRandom random = new SecureRandom();
+        RSAKeyGenerationParameters keyGenParams = new RSAKeyGenerationParameters(
+                publicExponent, random, 4096, 80
+        );
+
+        generator.init(keyGenParams);
+        return generator.generateKeyPair();
+    }
+
+    private String privateToPemString(AsymmetricCipherKeyPair keyPair) {
+        var pemStringWriter = new StringWriter();
+
+        try {
+            var privateKeyInfo = PrivateKeyInfoFactory.createPrivateKeyInfo(keyPair.getPrivate());
+
+            var pemObject = new PemObject("RSA PRIVATE KEY", privateKeyInfo.getEncoded());
+            var pemWriter = new PemWriter(pemStringWriter);
+            pemWriter.writeObject(pemObject);
+            pemWriter.close();
+
+            return pemStringWriter.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
