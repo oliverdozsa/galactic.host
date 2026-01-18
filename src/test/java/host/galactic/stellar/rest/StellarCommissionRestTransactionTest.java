acc0eabea02b02f6524d096173828730c73f388a
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-01-18 17:30:17 +0100

    Added even more tests for create transaction.

--- src/test/java/host/galactic/stellar/rest/StellarCommissionRestTransactionTest.java
+++ src/test/java/host/galactic/stellar/rest/StellarCommissionRestTransactionTest.java
@@ -3,8 +3,11 @@ package host.galactic.stellar.rest;
 import host.galactic.stellar.StellarBaseTest;
 import host.galactic.stellar.rest.requests.commission.CommissionCreateTransactionRequest;
 
+import host.galactic.stellar.rest.requests.commission.CommissionGetTransactionOfSignatureRequest;
 import host.galactic.stellar.rest.responses.commission.CommissionCreateTransactionResponse;
+import host.galactic.stellar.rest.responses.commission.CommissionGetTransactionOfSignatureResponse;
 import host.galactic.testutils.RsaEnvelope;
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.transaction.Transactional;
@@ -32,22 +35,15 @@ public class StellarCommissionRestTransactionTest extends StellarBaseTest {
 
     @Test
     public void testCreateTransaction() {
+        Log.info("[START TEST]: testCreateTransaction()");
+
         var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
 
         waitForAssetAccountsToBeCreatedFor(votingId);
         waitForChannelAccountsToBeCreatedFor(votingId);
 
         var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
-        var signingKeyPublic = rest.commission.getPublicKey().publicKey();
-
-        var rsaEnvelope = new RsaEnvelope(signingKeyPublic);
-        var envelopedMessage = rsaEnvelope.create(message.getBytes());
-        var base64Envelope = utils.toBase64(envelopedMessage);
-
-        var envelopeSignatureResponse = rest.commission.signEnvelope(base64Envelope);
-        var envelopeSignatureBytes = utils.fromBase64(envelopeSignatureResponse.signature());
-        var revealedSignature = rsaEnvelope.revealedSignature(envelopeSignatureBytes);
-        var revealedSignatureBase64 = utils.toBase64(revealedSignature);
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
 
         var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
         var response = given()
@@ -61,41 +57,177 @@ public class StellarCommissionRestTransactionTest extends StellarBaseTest {
                 .as(CommissionCreateTransactionResponse.class);
 
         assertThat(response.transaction(), not(blankOrNullString()));
+
+        Log.info("[  END TEST]: testCreateTransaction()");
     }
 
     @Test
     public void testCreateTransactionButNotEnoughChannelAccounts() {
-        fail("Implement testCreateTransactionButNotEnoughChannelAccounts()");
-    }
+        Log.info("[START TEST]: testCreateTransactionButNotEnoughChannelAccounts()");
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
 
-    @Test
-    public void testCreateTransactionButAssetAccountsAreNotYetCreated() {
-        fail("Implement testCreateTransactionButAssetAccountsAreNotYetCreated()");
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
+
+        var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+
+        Log.info("[  END TEST]: testCreateTransactionButNotEnoughChannelAccounts()");
     }
 
     @Test
     public void testCreateTransactionInvalidVotingId() {
-        fail("Implement testCreateTransactionInvalidVotingId()");
+        Log.info("[START TEST]: testCreateTransactionInvalidVotingId()");
+
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
+
+        waitForAssetAccountsToBeCreatedFor(votingId);
+        waitForChannelAccountsToBeCreatedFor(votingId);
+
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
+
+        var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        Log.info("[  END TEST]: testCreateTransactionInvalidVotingId()");
     }
 
     @Test
     public void testCreateTransactionButAlreadySentIt() {
-        fail("Implement testCreateTransactionButAlreadySentIt()");
+        Log.info("[START TEST]: testCreateTransactionButAlreadySentIt()");
+
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
+
+        waitForAssetAccountsToBeCreatedFor(votingId);
+        waitForChannelAccountsToBeCreatedFor(votingId);
+
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
+
+        var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        Log.info("[  END TEST]: testCreateTransactionButAlreadySentIt()");
     }
 
     @Test
     public void testGetTransactionForSignature() {
-        fail("Implement testGetTransactionForSignature()");
+        Log.info("[START TEST]: testGetTransactionForSignature()");
+
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
+
+        waitForAssetAccountsToBeCreatedFor(votingId);
+        waitForChannelAccountsToBeCreatedFor(votingId);
+
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
+
+        var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        var getTxOfSignatureRequest = new CommissionGetTransactionOfSignatureRequest(revealedSignatureBase64);
+        var getTxOfSignatureResponse = given()
+                .contentType(ContentType.JSON)
+                .body(getTxOfSignatureRequest)
+                .when()
+                .post(rest.commission.url + "/transactionofsignature/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract()
+                .body().as(CommissionGetTransactionOfSignatureResponse.class);
+
+        assertThat(getTxOfSignatureResponse.transaction(), not(blankOrNullString()));
+
+        Log.info("[  END TEST]: testGetTransactionForSignature()");
     }
 
     @Test
     public void testTryGettingNonExistingTransactionForSignature() {
-        fail("Implement testTryGettingNonExistingTransactionForSignature()");
+        Log.info("[START TEST]: testTryGettingNonExistingTransactionForSignature()");
+
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
+
+        waitForAssetAccountsToBeCreatedFor(votingId);
+        waitForChannelAccountsToBeCreatedFor(votingId);
+
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+        var revealedSignatureBase64 = createRevealedSignatureFor(message, votingId, "bob");
+
+        var request = new CommissionCreateTransactionRequest(message, revealedSignatureBase64);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        var getTxOfSignatureRequest = new CommissionGetTransactionOfSignatureRequest("someRandomSig");
+        given()
+                .contentType(ContentType.JSON)
+                .body(getTxOfSignatureRequest)
+                .when()
+                .post(rest.commission.url + "/transactionofsignature/")
+                .then()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+
+        Log.info("[  END TEST]: testTryGettingNonExistingTransactionForSignature()");
     }
 
     @Test
     public void testCreateTransactionButSignatureIsInvalid() {
-        fail("Implement testCreateTransactionButSignatureIsInvalid()");
+        Log.info("[START TEST]: testCreateTransactionButSignatureIsInvalid()");
+
+        var votingId = rest.voting.createWithParticipants("alice", new String[]{"bob", "charlie"});
+
+        waitForAssetAccountsToBeCreatedFor(votingId);
+        waitForChannelAccountsToBeCreatedFor(votingId);
+
+        var message = votingId + "|" + TEST_STELLAR_ACCOUNT;
+
+        var request = new CommissionCreateTransactionRequest(message, "someRandomSignature");
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(rest.commission.url + "/transaction/")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        Log.info("[  END TEST]: testCreateTransactionButSignatureIsInvalid()");
     }
 
     public void waitForChannelAccountsToBeCreatedFor(Long votingId) {
@@ -116,5 +248,16 @@ public class StellarCommissionRestTransactionTest extends StellarBaseTest {
         return db.areChannelAccountsCreatedFor(votingId);
     }
 
-    private String createSignatureFor(Long votingId)
+    private String createRevealedSignatureFor(String message, Long votingId, String user) {
+        var signingKeyPublic = rest.commission.getPublicKey().publicKey();
+
+        var rsaEnvelope = new RsaEnvelope(signingKeyPublic);
+        var envelopedMessage = rsaEnvelope.create(message.getBytes());
+        var base64Envelope = utils.toBase64(envelopedMessage);
+
+        var envelopeSignatureResponse = rest.commission.signEnvelope(base64Envelope, user, votingId);
+        var envelopeSignatureBytes = utils.fromBase64(envelopeSignatureResponse.signature());
+        var revealedSignature = rsaEnvelope.revealedSignature(envelopeSignatureBytes);
+        return utils.toBase64(revealedSignature);
+    }
 }
